53e8e4bb67b4a1269cc875b32334b2c118653e06
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-08-17 15:04:09 +0300

    Init commit.

---
+++ src/main/java/merkanto/sdjpawordpress/SdjpaWordPressApplication.java
@@ -0,0 +1,13 @@
+package merkanto.sdjpawordpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaWordPressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdjpaWordPressApplication.class, args);
+    }
+
+}
---
+++ src/test/java/merkanto/sdjpawordpress/SdjpaWordPressApplicationTests.java
@@ -0,0 +1,13 @@
+package merkanto.sdjpawordpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaWordPressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
